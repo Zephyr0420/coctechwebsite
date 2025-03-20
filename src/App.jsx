57df838792a0f8d2427cb27c3505491f9f9cb117
Joshua Rashtian <joshrashtian1@gmail.com>
2025-03-20 15:12:29 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div>
+      <h1>COC Tech Club</h1>
+      <p>Hello, world!</p>
+      <p className='testing'>Welcome to our College of the Canyons Tech Club! We build projects and teach the college of the canyons students all of the skills used in the Computer Science and Engineering World!</p>
+      <code>   Hello, world!</code>
+      <div>
+      <button><p>Join Now!</p></button>
+      </div>
+      
+    </div>
+     {/* <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p> */}
+    </>
+  )
+}
+
+export default App
